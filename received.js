21c06b93671415e1e239a199478e3e66410f6866
irfanit93 <irfanit93@gmail.com>
2017-11-24 01:06:57 +0530

    ngrokwebhook

--- received.js
+++ received.js
@@ -8,7 +8,7 @@ console.log('request received');
 res.send('Nodejsngrok repo');
 });
 app.post('/received',function(req,res){
-console.log('request received at received port 3000'+req.headers['X-Github-Event']);
+console.log('request received at received port 3000'+req.headers);
 res.send('Nodejsngrok repo'+req.body);
 });
 app.listen(3000,function(){
